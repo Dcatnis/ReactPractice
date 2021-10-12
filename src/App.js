1d6e8c29a664d324de7af17d448eeb4f6bc4fd89
Dcatnis <khirghizz@gmail.com>
2021-10-12 09:11:07 +0300

    seccond commit

--- src/App.js
+++ src/App.js
@@ -4,37 +4,38 @@ import React from 'react'
 import Header from "./Header/Header"
 import Footer from "./Footer/Footer"
 import CursSection from './CursSection/CursSection';
-import {BrowserRouter as Router,Switch,Route} from "react-router-dom"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import About from './About/About';
 
 
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props)
   }
-  render(){
+  render() {
 
     return (
       <div >
-        
-   <Header />
 
-   <Router>
-     <Switch>
-    <Route exact path="/" component={CursSection}/>
-    <Route exact path="/About" component={About}/>
-     </Switch> 
-</Router>
-    
+        <Header />
 
-    <Footer />
+        <Router>
+          <Switch>
+            <Route exact path="/" component={CursSection} />
+            <Route exact path="/About" component={About} />
+          </Switch>
+        </Router>
+
+
+        <Footer />
+        blab
 
       </div>
     );
   }
 
 
-  }
- 
+}
+
 
 export default App;
